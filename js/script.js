887510ab4aa08506c374c9ce79e0bfe43c9b014a
Brittney Cavazos <brittney.cavazos@gmail.com>
2021-07-01 20:36:27 -0700

    day 02 daily challenge

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+//Take Home
+//Day 01
+
 //This is a single line comment
-
 /* This is a multi-line comment */
-
 console.log('Brittney');
 console.log('curry');
+
+let sup='“Supercalifragilisticexpialidocious”'
+console.log(59);
+
+console.log(sup.length);
+
+console.log(typeof sup);
+console.log(typeof 21036786786786780099096998489390458905498068094843823485728456344556477);
